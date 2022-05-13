7b5b6266d1a58d73d316f0d8ab45d92b725fe907
Joelcoding <joelpott92@gmail.com>
2022-05-13 17:30:26 +0200

    Projektdateien hinzufügen.

---
+++ NettoBerechner/Program.cs
@@ -0,0 +1,25 @@
+﻿// Netto Rechner :) made by Joel
+
+Console.BackgroundColor = ConsoleColor.Black;
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("   Guten Tag geben sie ihren Namen an");
+string name = Console.ReadLine();
+Console.WriteLine("");
+Console.WriteLine("Gebe deinen Monatslohn an");
+float Monatslohn = Int32.Parse(Console.ReadLine());
+Console.WriteLine("");
+Console.WriteLine("Gebe deinen Jahreslohn an");
+float JahresLohn = Int32.Parse(Console.ReadLine());
+
+
+Monatslohn = (Monatslohn / 1.2f);
+JahresLohn = (JahresLohn / 1.2f);
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("   Guten Tag" + "." + name);
+Console.WriteLine("");
+Console.WriteLine("   Dein MonatsLohn Netto:" + Monatslohn);
+Console.WriteLine("");
+Console.WriteLine("   Dein JahresLohn Netto:" + JahresLohn);
+Console.ReadKey();
